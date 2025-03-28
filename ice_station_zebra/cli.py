a572a1989e07ff4b58fcfdfbbfcc8b44211bcf96
James Robinson <james.em.robinson@gmail.com>
2025-03-28 15:34:33 +0000

    :sparkles: Add hydra config reader

--- ice_station_zebra/cli.py
+++ ice_station_zebra/cli.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from ice-station-zebra!")
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
+@hydra.main(version_base=None, config_path="config", config_name="zebra")
+def main(cfg : DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
 
 if __name__ == "__main__":
     main()

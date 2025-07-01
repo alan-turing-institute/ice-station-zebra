3694430a99c97303ff0c063897e10c2c2fa31284
James Robinson <james.em.robinson@gmail.com>
2025-07-01 10:45:59 +0100

    :alien: Pin icenet to 0.2.7 as this is the latest version that does not constrain netcdf4 (as netcdf4<1.7.0 is not installable on macOS 15). Note that this means we may suffer from the multi-threading bug in netcdf4 so we *must* disable parallelism in all IceNet operations

--- ice_station_zebra/datasets/preprocessors/icenet_sic.py
+++ ice_station_zebra/datasets/preprocessors/icenet_sic.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 import pandas as pd
@@ -26,9 +27,14 @@ class IceNetSICPreprocessor(IPreprocessor):
         return not self.is_north
 
     def download(self, preprocessor_path: Path) -> None:
+        # Change to the output directory before downloading
         icenet_path = preprocessor_path / self.name
+        icenet_path.mkdir(parents=True, exist_ok=True)
+        current_directory = os.getcwd()
+        os.chdir(icenet_path)
+
         # Generate polar masks: note that only 1979-2015 are available
-        masks = Masks(north=self.is_north, south=self.is_south, path=icenet_path)
+        masks = Masks(north=self.is_north, south=self.is_south)
         mask_year = max(max(1979, min(date.year, 2015)) for date in self.date_range)
         log.info(f"Generating polar masks for {mask_year}...")
         masks.generate(year=mask_year)
@@ -41,8 +47,9 @@ class IceNetSICPreprocessor(IPreprocessor):
             delete_tempfiles=True,  # Delete temporary downloaded files after use
             north=self.is_north,  # Use mask for the Northern Hemisphere (set to True if needed)
             south=self.is_south,  # Use mask for the Southern Hemisphere
-            masks_path=icenet_path,  # Path to masks
             parallel_opens=False,  # Concatenation is not working correctly with dask
-            path=icenet_path,  # Path to download data
         )
         sic.download()
+
+        # Change back to the original directory
+        os.chdir(current_directory)

af671f8ce9f20c942202a6ad2ed9e6bbeb2c1351
James Robinson <james.em.robinson@gmail.com>
2025-07-17 09:11:07 +0100

    :coffin: Remove anemoi-training infrastructure

--- ice_station_zebra/training/anemoi_adaptors/__init__.py
+++
@@ -1,7 +0,0 @@
-from .non_gridded_datamodule import NonGriddedDataModule
-from .trainer_config import TrainerConfigAdaptor
-
-__all__ = [
-    "NonGriddedDataModule",
-    "TrainerConfigAdaptor",
-]

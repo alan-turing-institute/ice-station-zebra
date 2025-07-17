62aa6c9c4a016f85343498729159b5b78bb50b29
James Robinson <james.em.robinson@gmail.com>
2025-07-17 15:10:38 +0100

    :label: Enforce type conversion with pydantic

--- ice_station_zebra/types.py
+++ ice_station_zebra/types.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import TypedDict
 
+from pydantic import validate_arguments
+
 
 class DataloaderArgs(TypedDict):
     batch_size: int
@@ -10,7 +12,12 @@ class DataloaderArgs(TypedDict):
     worker_init_fn: None
 
 
+@validate_arguments
 @dataclass
 class ZebraDataSpace:
     channels: int
     shape: tuple[int, int]
+
+    def __post_init__(self):
+        if not isinstance(self.shape, tuple):
+            self.shape = tuple(self.shape)

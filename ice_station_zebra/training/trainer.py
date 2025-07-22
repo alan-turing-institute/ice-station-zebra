7e2ba15c8840f9453c3a2b91017a14f5036bdd61
James Robinson <james.em.robinson@gmail.com>
2025-07-22 20:58:54 +0100

    :truck: Save callbacks and model config to the wandb run folder

--- ice_station_zebra/training/trainer.py
+++ ice_station_zebra/training/trainer.py
@@ -1,10 +1,13 @@
 import logging
 from collections import defaultdict
+from contextlib import suppress
+from datetime import datetime
 from pathlib import Path
 
 import hydra
-from lightning import Trainer
+from lightning import Callback, Trainer
 from omegaconf import DictConfig, OmegaConf
+from wandb.sdk.lib.runid import generate_id
 
 from .data_modules import ZebraDataModule
 
@@ -66,16 +69,34 @@ class ZebraTrainer:
             for logger_config in config.get("loggers", {}).values()
         ]
 
+        # Get run directory from wandb logger if available
+        run_directory = None
+        for lightning_logger in lightning_loggers:
+            with suppress(AttributeError):
+                run_directory = Path(lightning_logger.experiment._settings.sync_dir)
+        if not run_directory:
+            name_ = f"run-{datetime.now().strftime(r'%Y%m%d_%H%M%S')}-{generate_id()}"
+            run_directory = base_path / "training" / "local" / name_
+
+        # Add callbacks
+        callbacks: list[Callback] = []
+        for callback_cfg in config["train"].get("callbacks", {}).values():
+            callbacks.append(hydra.utils.instantiate(callback_cfg))
+
         # Construct the trainer
         self.trainer: Trainer = hydra.utils.instantiate(
             dict(
                 {
-                    "default_root_dir": base_path / "training",
+                    "callbacks": callbacks,
                     "logger": lightning_loggers,
                 },
                 **config["train"]["trainer"],
             )
         )
+        self.trainer.checkpoint_callback.dirpath = run_directory / "checkpoints"
+
+        # Save config to the output directory
+        OmegaConf.save(config, run_directory / "model_config.yaml")
 
     def train(self) -> None:
         self.trainer.fit(

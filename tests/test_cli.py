135e918192eef30438757a8545c92e05d72cdc44
James Robinson <james.em.robinson@gmail.com>
2025-07-25 15:52:41 +0100

    :tada: Initial testing framework

---
+++ tests/test_cli.py
@@ -0,0 +1,18 @@
+import re
+
+from typer.testing import CliRunner
+
+from ice_station_zebra.cli import app
+
+runner = CliRunner()
+
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    expected_patterns = [
+        "│ datasets\s+Manage datasets",
+        "│ train",
+    ]
+    lines = result.output.split("\n")
+    for pattern in expected_patterns:
+        assert any([re.search(pattern, line) for line in lines])

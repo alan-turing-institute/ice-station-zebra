4d10da1a2e997d6b3dc2af5b9ceed4d819c10b43
James Robinson <james.em.robinson@gmail.com>
2025-07-29 16:24:28 +0100

    :truck: Refactored tests into classes and ensured that executable name is correctly set

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,74 +1,105 @@
 import re
 
-from click.testing import Result
+from typer import Typer
 from typer.testing import CliRunner
 
 from ice_station_zebra.cli import app
 
-runner = CliRunner()
+
+class ZebraCliRunner(CliRunner):
+    def __init__(self, app: Typer) -> None:
+        """A custom CLI runner for Zebra tests"""
+        super().__init__()
+        self.app = app
+        self.colorstrip = re.compile(r"\x1b\[[0-9;]*m")
+
+    def output(self, commands: list[str]) -> list[str]:
+        """Invoke the CLI commands and return the output as a list of strings."""
+        result = super().invoke(self.app, commands, prog_name="zebra")
+        assert result.exit_code == 0, (
+            f"Command failed with exit code {result.exit_code}: {result.output}"
+        )
+        if result.exception:
+            raise result.exception
+        return [self.colorstrip.sub("", line) for line in result.output.split("\n")]
+
+    def check_output(self, commands: list[str], expected_patterns: list[str]) -> None:
+        """Check if the output contains all expected patterns."""
+        output = self.output(commands)
+        for pattern in expected_patterns:
+            found_match = any(re.search(pattern, line) for line in output)
+            assert found_match, f"Pattern '{pattern}' not found in output"
 
 
-def plain_lines(result: Result) -> list[str]:
-    """Helper to strip colour codes from the output."""
-    colorstrip = re.compile(r"\x1b[^m]*m")
-    return [colorstrip.sub("", line) for line in result.output.split("\n")]
-
-
-def test_help():
-    result = runner.invoke(app, ["--help"])
-    expected_patterns = [
-        r"Usage: zebra [OPTIONS] COMMAND [ARGS]...",
+class TestBaseCLI:
+    expected_patterns_help = [
+        r"Usage: zebra \[OPTIONS\] COMMAND \[ARGS\]...",
         r"Entrypoint for zebra application commands",
         r"--install-completion\s+Install completion for the current shell.",
-        r"--show-completion\s+Show completion for the current shell, to copy it or customize the installation.",
+        r"--show-completion\s+Show completion for the current shell",
         r"--help\s+-h\s+Show this message and exit.",
         r"datasets\s+Manage datasets",
         r"evaluate\s+Evaluate a model",
         r"train\s+Train a model",
     ]
-    lines = plain_lines(result)
-    for pattern in expected_patterns:
-        assert any([re.search(pattern, line) for line in lines])
+
+    def test_help(self):
+        runner = ZebraCliRunner(app)
+        runner.check_output(
+            ["--help"],
+            expected_patterns=self.expected_patterns_help,
+        )
+
+    def test_short_help(self):
+        runner = ZebraCliRunner(app)
+        runner.check_output(
+            ["-h"],
+            expected_patterns=self.expected_patterns_help,
+        )
 
 
-def test_datasets_help():
-    result = runner.invoke(app, ["datasets", "--help"])
-    expected_patterns = [
-        r"Usage: zebra datasets [OPTIONS] COMMAND [ARGS]...",
-        r"Manage datasets",
-        r"--help\s+-h\s+Show this message and exit.",
-        r"create\s+Create all datasets",
-        r"inspect\s+Inspect all datasets",
-    ]
-    lines = plain_lines(result)
-    for pattern in expected_patterns:
-        assert any([re.search(pattern, line) for line in lines])
+class TestDatasetsCLI:
+    def test_help(self):
+        runner = ZebraCliRunner(app)
+        runner.check_output(
+            ["datasets", "--help"],
+            expected_patterns=[
+                r"Usage: zebra datasets \[OPTIONS\] COMMAND \[ARGS\]...",
+                r"Manage datasets",
+                r"--help\s+-h\s+Show this message and exit.",
+                r"create\s+Create all datasets",
+                r"inspect\s+Inspect all datasets",
+            ],
+        )
 
 
-def test_evaluate_help():
-    result = runner.invoke(app, ["train", "--help"])
-    expected_patterns = [
-        r"Usage: zebra evaluate [OPTIONS] [OVERRIDES]...",
-        r"Evaluate a model",
-        r"overrides\s+[OVERRIDES]...\s+Apply space-separated Hydra config overrides (https://hydra.cc/docs/advanced/override_grammar/basic/) [default: None]",
-        r"--checkpoint\s+TEXT\s+Specify the path to a trained model checkpoint [default: None] [required]",
-        r"--config-name\s+TEXT\s+Specify the name of a file to load from the config directory [default: zebra]",
-        r"--help\s+-h\s+Show this message and exit.",
-    ]
-    lines = plain_lines(result)
-    for pattern in expected_patterns:
-        assert any([re.search(pattern, line) for line in lines])
+class TestEvaluateCLI:
+    def test_help(self):
+        runner = ZebraCliRunner(app)
+        runner.check_output(
+            ["evaluate", "--help"],
+            expected_patterns=[
+                r"Usage: zebra evaluate \[OPTIONS\] \[OVERRIDES\]...",
+                r"Evaluate a model",
+                r"overrides\s+\[OVERRIDES\]...\s+Apply space-separated Hydra config",
+                r"--checkpoint\s+TEXT\s+Specify the path to a trained model",
+                r"--config-name\s+TEXT\s+Specify the name of a file to load from the",
+                r"--help\s+-h\s+Show this message and exit.",
+            ],
+        )
 
 
-def test_train_help():
-    result = runner.invoke(app, ["train", "--help"])
-    expected_patterns = [
-        r"Usage: zebra train [OPTIONS] [OVERRIDES]...",
-        r"Train a model",
-        r"overrides\s+[OVERRIDES]...\s+Apply space-separated Hydra config overrides (https://hydra.cc/docs/advanced/override_grammar/basic/) [default: None]",
-        r"--config-name\s+TEXT\s+Specify the name of a file to load from the config directory [default: zebra]",
-        r"--help\s+-h\s+Show this message and exit.",
-    ]
-    lines = plain_lines(result)
-    for pattern in expected_patterns:
-        assert any([re.search(pattern, line) for line in lines])
+class TestTrainCLI:
+    def test_train_help(self):
+        # found_match = runner.invoke(app, ["train", "--help"])
+        runner = ZebraCliRunner(app)
+        runner.check_output(
+            ["train", "--help"],
+            expected_patterns=[
+                r"Usage: zebra train \[OPTIONS\] \[OVERRIDES\]...",
+                r"Train a model",
+                r"overrides\s+\[OVERRIDES\]...\s+Apply space-separated Hydra config",
+                r"--config-name\s+TEXT\s+Specify the name of a file to load from the",
+                r"--help\s+-h\s+Show this message and exit.",
+            ],
+        )

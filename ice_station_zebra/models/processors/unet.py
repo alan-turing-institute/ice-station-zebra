fdc2a69bfb8908567dffda42e4928b46c16fc0c9
Isabel Fenton <ifenton@turing.ac.uk>
2025-08-04 16:29:44 +0100

    Converting channels from dict to list

--- ice_station_zebra/models/processors/unet.py
+++ ice_station_zebra/models/processors/unet.py
@@ -12,44 +12,43 @@ class UNetProcessor(nn.Module):
         n_latent_channels: int,
         filter_size=3,
         n_filters_factor=1,
-        n_forecast_days=7,
+        #n_forecast_days=7,
     ) -> None:
         super().__init__()
 
         start_out_channels = 64
-
+        
         reduced_channels = int(start_out_channels * n_filters_factor)
-        channels = {
-            start_out_channels * 2**pow: reduced_channels * 2**pow for pow in range(4)
-        }
+        
+        channels = [reduced_channels * 2**pow for pow in range(4)]
 
         # Encoder
-        self.conv1 = ConvBlock(n_latent_channels, channels[64], filter_size=filter_size)
-        self.conv2 = ConvBlock(channels[64], channels[128], filter_size=filter_size)
-        self.conv3 = ConvBlock(channels[128], channels[256], filter_size=filter_size)
-        self.conv4 = ConvBlock(channels[256], channels[256], filter_size=filter_size)
+        self.conv1 = ConvBlock(n_latent_channels, channels[0], filter_size=filter_size)
+        self.conv2 = ConvBlock(channels[0], channels[1], filter_size=filter_size)
+        self.conv3 = ConvBlock(channels[1], channels[2], filter_size=filter_size)
+        self.conv4 = ConvBlock(channels[2], channels[2], filter_size=filter_size)
 
         # Bottleneck
         self.conv5 = BottleneckBlock(
-            channels[256], channels[512], filter_size=filter_size
+            channels[2], channels[3], filter_size=filter_size
         )
 
         # Decoder
-        self.up6 = UpconvBlock(channels[512], channels[256])
-        self.up7 = UpconvBlock(channels[256], channels[256])
-        self.up8 = UpconvBlock(channels[256], channels[128])
-        self.up9 = UpconvBlock(channels[128], channels[64])
+        self.up6 = UpconvBlock(channels[3], channels[2])
+        self.up7 = UpconvBlock(channels[2], channels[2])
+        self.up8 = UpconvBlock(channels[2], channels[1])
+        self.up9 = UpconvBlock(channels[1], channels[0])
 
-        self.up6b = ConvBlock(channels[512], channels[256], filter_size=filter_size)
-        self.up7b = ConvBlock(channels[512], channels[256], filter_size=filter_size)
-        self.up8b = ConvBlock(channels[256], channels[128], filter_size=filter_size)
+        self.up6b = ConvBlock(channels[3], channels[2], filter_size=filter_size)
+        self.up7b = ConvBlock(channels[3], channels[2], filter_size=filter_size)
+        self.up8b = ConvBlock(channels[2], channels[1], filter_size=filter_size)
         self.up9b = ConvBlock(
-            channels[128], channels[64], filter_size=filter_size, final=True
+            channels[1], channels[0], filter_size=filter_size, final=True
         )
 
         # Final layer
         self.final_layer = nn.Conv2d(
-            channels[64], n_latent_channels, kernel_size=1, padding="same"
+            channels[0], n_latent_channels, kernel_size=1, padding="same"
         )
 
     def forward(self, x: Tensor) -> Tensor:
@@ -64,19 +63,19 @@ class UNetProcessor(nn.Module):
         conv3 = F.max_pool2d(bn3, kernel_size=2)
         bn4 = self.conv4(conv3)
         conv4 = F.max_pool2d(bn4, kernel_size=2)
-
+        
         # Bottleneck
         bn5 = self.conv5(conv4)
-
+        
         # Decoder
         up6 = self.up6b(torch.cat([bn4, self.up6(bn5)], dim=1))
         up7 = self.up7b(torch.cat([bn3, self.up7(up6)], dim=1))
         up8 = self.up8b(torch.cat([bn2, self.up8(up7)], dim=1))
         up9 = self.up9b(torch.cat([bn1, self.up9(up8)], dim=1))
-
+        
         # Final layer
         output = self.final_layer(up9)
-
+        
         return output
 
 

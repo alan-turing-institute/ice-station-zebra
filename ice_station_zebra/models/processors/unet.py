eafc34a7574e06df44f967ae023546986372cc85
Isabel Fenton <IFenton@users.noreply.github.com>
2025-08-04 12:45:21 +0100

    Remove redundant class
    Co-authored-by: James Robinson <james.em.robinson@gmail.com>

--- ice_station_zebra/models/processors/unet.py
+++ ice_station_zebra/models/processors/unet.py
@@ -80,18 +80,6 @@ class UNetProcessor(nn.Module):
         return output
 
 
-# class Interpolate(nn.Module):
-#     def __init__(self, scale_factor, mode):
-#         super().__init__()
-#         self.interp = F.interpolate
-#         self.scale_factor = scale_factor
-#         self.mode = mode
-
-#     def forward(self, x):
-#         x = self.interp(x, scale_factor=self.scale_factor, mode=self.mode)
-#         return x
-
-
 class ConvBlock(nn.Module):
     def __init__(
         self,

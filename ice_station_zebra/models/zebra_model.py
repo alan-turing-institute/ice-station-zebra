16c1b512334d9e632155a28d8024fb70573e6053
James Robinson <james.em.robinson@gmail.com>
2025-08-08 11:41:42 +0100

    Fix the model summary table (#50)
    * :bug: Use the implicit self._modules instead of a manual ModuleList to keep track of child modules. This fixes the incorrectly displayed summary table
    
    * :art: Simplify even further by inlining the parameters() method
    
    * :alien: Depend on public API rather than a private attribute
    
    * :bug: Update name to avoid shadowing

--- ice_station_zebra/models/zebra_model.py
+++ ice_station_zebra/models/zebra_model.py
@@ -1,8 +1,8 @@
+import itertools
 from abc import ABC, abstractmethod
 
 import hydra
 import torch
-import torch.nn as nn
 from lightning import LightningModule
 from omegaconf import DictConfig
 from torch.optim import Optimizer
@@ -28,9 +28,6 @@ class ZebraModel(LightningModule, ABC):
         self.input_spaces = [DataSpace.from_dict(space) for space in input_spaces]
         self.output_space = DataSpace.from_dict(output_space)
 
-        # Initialise an empty module list
-        self.model_list = nn.ModuleList()
-
         # Store the optimizer config
         self.optimizer_cfg = optimizer
 
@@ -46,7 +43,12 @@ class ZebraModel(LightningModule, ABC):
     def configure_optimizers(self) -> Optimizer:
         """Construct the optimizer from the config"""
         return hydra.utils.instantiate(
-            dict(**self.optimizer_cfg) | {"params": self.model_list.parameters()}
+            dict(**self.optimizer_cfg)
+            | {
+                "params": itertools.chain(
+                    *[module.parameters() for module in self.children()]
+                )
+            }
         )
 
     def loss(self, output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:

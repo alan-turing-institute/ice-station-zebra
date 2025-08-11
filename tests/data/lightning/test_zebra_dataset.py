a68a69b077266908950284c42b94d6b17d1d5d20
James Robinson <james.em.robinson@gmail.com>
2025-08-11 15:28:26 +0100

    :white_check_mark: Add tests for a dummy AnemoiDataset

--- tests/data/lightning/test_zebra_dataset.py
+++ tests/data/lightning/test_zebra_dataset.py
@@ -1,16 +1,42 @@
-from pathlib import Path
-from ice_station_zebra.data.lightning.zebra_dataset import ZebraDataset
+import pytest
 import numpy as np
+
+from ice_station_zebra.data.lightning.zebra_dataset import ZebraDataset
+from ice_station_zebra.types import DataSpace
+
+
+class MockAnemoiDataset:
+    def __init__(self):
+        self.name = "mock_dataset"
+        self.start_date = np.datetime64("2020-01-01")
+        self.end_date = np.datetime64("2020-12-31")
+        self.field_shape = (5, 5)
+        self.shape = [1, 10]  # (time, channels)
+
+    def __len__(self):
+        return 10
 
 
 class TestZebraDataset:
     def test_init(self) -> None:
         dataset = ZebraDataset(
             name="test_dataset",
-            input_files=[Path("dummy.nc")],
-            start="1993-12-01",
-            end="1993-12-31",
+            input_files=[],
         )
-        assert dataset.start_date == np.datetime64("1993-12-01")
-        assert dataset.end_date == np.datetime64("1993-12-31")
         assert dataset.name == "test_dataset"
+
+    def test_dataset(self, monkeypatch: pytest.MonkeyPatch) -> None:
+        monkeypatch.setattr(
+            ZebraDataset, "dataset", property(lambda _: MockAnemoiDataset())
+        )
+        dataset = ZebraDataset(
+            name="mock_dataset",
+            input_files=[],
+        )
+        assert dataset.end_date == np.datetime64("2020-12-31")
+        assert dataset.name == "mock_dataset"
+        assert isinstance(dataset.space, DataSpace)
+        assert dataset.space.channels == 10
+        assert dataset.space.shape == (5, 5)
+        assert dataset.start_date == np.datetime64("2020-01-01")
+        assert len(dataset) == 10

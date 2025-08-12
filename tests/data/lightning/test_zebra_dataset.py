7b471fd6b9393ecaa4180ac5847754c2a83a35f7
James Robinson <james.em.robinson@gmail.com>
2025-08-12 10:58:16 +0100

    :white_check_mark: Add tests for DataSpace and length

--- tests/data/lightning/test_zebra_dataset.py
+++ tests/data/lightning/test_zebra_dataset.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 
 from ice_station_zebra.data.lightning.zebra_dataset import ZebraDataset
+from ice_station_zebra.types import DataSpace
 
 
 class TestZebraDataset:
@@ -31,6 +32,23 @@ class TestZebraDataset:
         )
         assert dataset.start_date == self.dates_np[0]
         assert dataset.end_date == self.dates_np[1]
+
+    def test_dataset_len(self, mock_dataset: Path) -> None:
+        dataset = ZebraDataset(
+            name="mock_dataset",
+            input_files=[mock_dataset],
+        )
+        assert len(dataset) == 3
+
+    def test_dataset_space(self, mock_dataset: Path) -> None:
+        dataset = ZebraDataset(
+            name="mock_dataset",
+            input_files=[mock_dataset],
+        )
+        # Test data space
+        assert isinstance(dataset.space, DataSpace)
+        assert dataset.space.channels == 1
+        assert dataset.space.shape == (2, 2)
 
     def test_dataset_start_date(self, mock_dataset: Path) -> None:
         dataset = ZebraDataset(

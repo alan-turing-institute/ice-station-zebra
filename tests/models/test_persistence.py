45dd909ea6de55d16937a429e0c6dedebf3faaf3
James Robinson <james.em.robinson@gmail.com>
2025-08-19 18:18:40 +0100

    :white_check_mark: Add simple test for Persistence

---
+++ tests/models/test_persistence.py
@@ -0,0 +1,58 @@
+import pytest
+import torch
+
+from ice_station_zebra.models import Persistence
+
+
+@pytest.mark.parametrize(
+    "test_input_shape", [(512, 512, 4), (1000, 200, 1), (1, 1, 20)]
+)
+@pytest.mark.parametrize("test_output_shape", [(432, 432, 1), (10, 20, 19), (1, 1, 1)])
+@pytest.mark.parametrize("test_batch_size", [1, 2, 5])
+@pytest.mark.parametrize("test_n_forecast_steps", [1, 2, 5])
+@pytest.mark.parametrize("test_n_history_steps", [1, 2, 5])
+class TestNullProcessor:
+    def test_forward_shape(
+        self,
+        test_batch_size: int,
+        test_input_shape: tuple[int, int, int],
+        test_n_forecast_steps: int,
+        test_n_history_steps: int,
+        test_output_shape: tuple[int, int, int],
+    ) -> None:
+        input_space = {
+            "channels": test_input_shape[2],
+            "name": "input",
+            "shape": test_input_shape[0:2],
+        }
+        output_space = {
+            "channels": test_output_shape[2],
+            "name": "target",
+            "shape": test_output_shape[0:2],
+        }
+        model = Persistence(
+            name="persistence",
+            input_spaces=[input_space],
+            n_forecast_steps=test_n_forecast_steps,
+            n_history_steps=test_n_history_steps,
+            output_space=output_space,
+            optimizer={},
+        )
+        batch = {
+            "input": torch.randn(
+                test_batch_size,
+                test_n_history_steps,
+                test_input_shape[2],
+                test_input_shape[0],
+                test_input_shape[1],
+            ),
+            "target": torch.randn(
+                test_batch_size,
+                test_n_forecast_steps,
+                test_output_shape[2],
+                test_output_shape[0],
+                test_output_shape[1],
+            ),
+        }
+        result: torch.Tensor = model(batch)
+        assert result.shape == batch["target"].shape

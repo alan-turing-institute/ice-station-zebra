e240d14fc41be1fbc8e798b36cf0414e36adf683
James Robinson <james.em.robinson@gmail.com>
2025-08-21 16:04:56 +0100

    :sparkles: Make decay rate configurable

--- ice_station_zebra/callbacks/ema_weight_averaging_callback.py
+++ ice_station_zebra/callbacks/ema_weight_averaging_callback.py
@@ -1,3 +1,5 @@
+from torch.optim.swa_utils import get_ema_multi_avg_fn
+
 from .weight_averaging import WeightAveraging
 
 
@@ -5,16 +7,23 @@ class EMAWeightAveragingCallback(WeightAveraging):
     """A callback that updates an averaged model for Exponential Moving Average (EMA) after each training step."""
 
     def __init__(
-        self, *, every_n_epochs: int | None = None, every_n_steps: int | None = None
+        self,
+        *,
+        decay_rate: float,
+        every_n_epochs: int | None = None,
+        every_n_steps: int | None = None,
     ) -> None:
         """Summarise metrics during evaluation.
 
         Args:
+            decay_rate: Parameter update decay rate.
             every_n_epochs: How many epochs to wait before updating.
             every_n_steps: How many steps to wait before updating.
 
         """
-        super().__init__()
+        super().__init__(
+            multi_avg_fn=get_ema_multi_avg_fn(decay_rate), use_buffers=True
+        )
         self.every_n_epochs = every_n_epochs
         self.every_n_steps = every_n_steps
 

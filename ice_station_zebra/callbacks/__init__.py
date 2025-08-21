d92025f2f7c17df373c30f2813c39e69aee9ae0a
James Robinson <james.em.robinson@gmail.com>
2025-08-21 14:03:00 +0100

    :alien: pytorch WeightAveraging callback which will become available in pytorch-lightning 2.5.4

--- ice_station_zebra/callbacks/__init__.py
+++ ice_station_zebra/callbacks/__init__.py
@@ -1,5 +1,11 @@
 from .metric_summary_callback import MetricSummaryCallback
 from .plotting_callback import PlottingCallback
 from .unconditional_checkpoint import UnconditionalCheckpoint
+from .weight_averaging import WeightAveraging
 
-__all__ = ["MetricSummaryCallback", "PlottingCallback", "UnconditionalCheckpoint"]
+__all__ = [
+    "MetricSummaryCallback",
+    "PlottingCallback",
+    "UnconditionalCheckpoint",
+    "WeightAveraging",
+]

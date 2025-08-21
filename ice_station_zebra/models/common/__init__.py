b9af40fc5f338f5f4a9e4ead5c33699f86543103
Maria Novitasari <mnovitasari@turing.ac.uk>
2025-08-21 16:17:44 +0100

    :hammer: Update init to include TimeEmbed

--- ice_station_zebra/models/common/__init__.py
+++ ice_station_zebra/models/common/__init__.py
@@ -1,9 +1,11 @@
 from .bottleneckblock import BottleneckBlock
 from .convblock import ConvBlock
 from .upconvblock import UpconvBlock
+from .timeembed import TimeEmbed
 
 __all__ = [
     "BottleneckBlock",
     "ConvBlock",
     "UpconvBlock",
+    "TimeEmbed",
 ]

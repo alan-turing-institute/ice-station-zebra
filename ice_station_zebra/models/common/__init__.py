3792c73f277e06a1354bdb87266a8c76c3bcc150
James Robinson <james.em.robinson@gmail.com>
2025-09-04 16:11:24 +0100

    :sparkles: Add an upsampling ConvBlock class

--- ice_station_zebra/models/common/__init__.py
+++ ice_station_zebra/models/common/__init__.py
@@ -1,5 +1,6 @@
 from .bottleneckblock import BottleneckBlock
 from .conv_block_downsample import ConvBlockDownsample
+from .conv_block_upsample import ConvBlockUpsample
 from .convblock import ConvBlock
 from .resizing_average_pool_2d import ResizingAveragePool2d
 from .timeembed import TimeEmbed
@@ -9,6 +10,7 @@ __all__ = [
     "BottleneckBlock",
     "ConvBlock",
     "ConvBlockDownsample",
+    "ConvBlockUpsample",
     "ResizingAveragePool2d",
     "TimeEmbed",
     "UpconvBlock",
--- ice_station_zebra/models/common/conv_block_upsample.py
+++ ice_station_zebra/models/common/conv_block_upsample.py
@@ -5,16 +5,16 @@ from ice_station_zebra.types import TensorNCHW
 from .activations import ACTIVATION_FROM_NAME
 
 
-class ConvBlockDownsample(nn.Module):
-    """Convolutional block that halves the resolution and doubles the number of channels.
+class ConvBlockUpsample(nn.Module):
+    """Convolutional block that doubles the resolution and halves the number of channels.
 
-    This is the reverse of ConvBlockUpsample.
+    This is the reverse of ConvBlockDownsample.
     """
 
     def __init__(
         self, n_input_channels: int, *, activation: str = "ReLU", kernel_size: int = 3
     ) -> None:
-        """Initialize the ConvBlockDownsample module.
+        """Initialize the ConvBlockUpsample module.
 
         Args:
             activation: the activation function to use.
@@ -26,14 +26,16 @@ class ConvBlockDownsample(nn.Module):
         activation_layer = ACTIVATION_FROM_NAME[activation]
 
         # Calculate convolutional parameters
-        n_output_channels = n_input_channels * 2
+        n_output_channels = n_input_channels // 2
         padding = (kernel_size - 1) // 2
+        output_padding = kernel_size % 2
 
         self.model = nn.Sequential(
-            nn.Conv2d(
+            nn.ConvTranspose2d(
                 n_input_channels,
                 n_output_channels,
                 kernel_size=kernel_size,
+                output_padding=output_padding,
                 padding=padding,
                 stride=2,
             ),

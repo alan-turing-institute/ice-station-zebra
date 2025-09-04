2b11e2e3294c0790bb3a5e1d734a8c757c442f8a
James Robinson <james.em.robinson@gmail.com>
2025-09-04 12:23:46 +0100

    :sparkles: Add a ResizingAveragePool2d class

--- ice_station_zebra/models/common/__init__.py
+++ ice_station_zebra/models/common/__init__.py
@@ -1,11 +1,13 @@
 from .bottleneckblock import BottleneckBlock
 from .convblock import ConvBlock
+from .resizing_average_pool_2d import ResizingAveragePool2d
 from .timeembed import TimeEmbed
 from .upconvblock import UpconvBlock
 
 __all__ = [
     "BottleneckBlock",
     "ConvBlock",
+    "ResizingAveragePool2d",
     "TimeEmbed",
     "UpconvBlock",
 ]

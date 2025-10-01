b9c838ab4c2e5cd7fb96f82c6bbb702e73e82969
erinuclkwon <72717299+erinuclkwon@users.noreply.github.com>
2025-10-01 14:40:46 +0100

    Adding Vision Transformer model to the pipeline (#85)
    * Initial vit processor and related modules in common
    
    Co-authored-by: James Robinson <james.em.robinson@gmail.com>
    Co-authored-by: Isabel Fenton <ifenton@turing.ac.uk>
    
    * tidy up before PR
    
    * fix minor inconsistency
    
    * :art: Tidying common block imports
    
    * Fix assumption H_patches==W_patches and remove unused model attributes
    
    * Fix linting issues and redundant model attributes
    
    * Changes in  processors/__init__.py
    
    * changing model size
    
    * fix merge conflicts
    
    * reordering
    
    * reordering
    
    ---------
    
    Co-authored-by: James Robinson <james.em.robinson@gmail.com>
    Co-authored-by: Isabel Fenton <ifenton@turing.ac.uk>
    Co-authored-by: Isabel Fenton <IFenton@users.noreply.github.com>

--- ice_station_zebra/models/common/__init__.py
+++ ice_station_zebra/models/common/__init__.py
@@ -2,16 +2,20 @@ from .conv_block_common import CommonConvBlock
 from .conv_block_downsample import ConvBlockDownsample
 from .conv_block_upsample import ConvBlockUpsample
 from .conv_block_upsample_naive import ConvBlockUpsampleNaive
+from .patchembed import PatchEmbedding
 from .resizing_average_pool_2d import ResizingAveragePool2d
 from .resizing_interpolation import ResizingInterpolation
 from .time_embed import TimeEmbed
+from .transformerblock import TransformerEncoderBlock
 
 __all__ = [
     "CommonConvBlock",
     "ConvBlockDownsample",
     "ConvBlockUpsample",
     "ConvBlockUpsampleNaive",
+    "PatchEmbedding",
     "ResizingAveragePool2d",
     "ResizingInterpolation",
     "TimeEmbed",
+    "TransformerEncoderBlock",
 ]
---
+++ ice_station_zebra/models/processors/vit.py
@@ -0,0 +1,96 @@
+"""Vision Transformer implementation.
+
+Author: Erin Quan
+
+Description:
+    Vision Transformer (ViT) model for sea ice forecasting that predicts future sea ice
+    concentration from meteorological data. Takes multi-channel input images, converts
+    them to patch embeddings, processes through transformer encoder blocks, and outputs
+    spatially-resolved predictions for specified forecast horizons.
+"""
+
+import torch
+from torch import nn
+
+from ice_station_zebra.models.common import PatchEmbedding, TransformerEncoderBlock
+from ice_station_zebra.types import TensorNCHW
+
+
+class VitProcessor(nn.Module):
+    def __init__(  # noqa: PLR0913
+        self,
+        n_latent_channels: int,
+        start_out_channels: int,
+        img_size: int,
+        patch_size: int,
+        emb_dim: int,
+        depth: int,
+        heads: int,
+        mlp_dim: int,
+        dropout: float,
+    ) -> None:
+        """Initialize Vision Transformer model for sea ice forecasting."""
+        super().__init__()
+
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.out_channels = start_out_channels
+
+        self.patch_embed = PatchEmbedding(
+            n_latent_channels, patch_size, emb_dim, self.img_size
+        )
+        num_patches = (self.img_size // patch_size) ** 2
+
+        self.pos_embed = nn.Parameter(torch.randn(1, num_patches, emb_dim))
+        self.dropout = nn.Dropout(dropout)
+
+        self.transformer = nn.Sequential(
+            *[
+                TransformerEncoderBlock(emb_dim, heads, mlp_dim, dropout)
+                for _ in range(depth)
+            ]
+        )
+
+        self.norm = nn.LayerNorm(emb_dim)
+
+        # (B, N, patch_size * patch_size * out_channels *)
+        self.decoder = nn.Sequential(
+            nn.Linear(emb_dim, patch_size * patch_size * self.out_channels),
+        )
+
+    def forward(self, x: TensorNCHW) -> TensorNCHW:
+        """Forward pass through the ViT model for sea ice forecasting before decoder.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape [B, C, H, W]
+
+        Returns:
+            torch.Tensor: Output tensor of shape [B, C, H, W]
+
+        """
+        batch, _, height, _ = x.shape
+
+        x = self.patch_embed(x)  # (B, N, D)
+        x = x + self.pos_embed
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+        x = self.norm(x)  # (B, N, D)
+        x = self.decoder(x)  # (B, N, out_channels**patch_size*patch_size)
+
+        h_patches = w_patches = height // self.patch_size
+        x = x.reshape(
+            batch,
+            h_patches,
+            w_patches,
+            self.out_channels,
+            self.patch_size,
+            self.patch_size,
+        )
+        x = x.permute(
+            0, 3, 1, 4, 2, 5
+        )  # (batch, out_channels, h_patches, patch_size, w_patches, patch_size)
+
+        return x.reshape(
+            batch, self.out_channels, self.img_size, self.img_size
+        )  # (B, C, H, W)

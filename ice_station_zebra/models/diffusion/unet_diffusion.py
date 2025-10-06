e40c9e6374ec34e5898a943c2f5cdbe60f8677ec
Maria Novitasari <mnovitasari@turing.ac.uk>
2025-10-06 12:51:09 +0100

    :hammer: Modify unet diffusion

--- ice_station_zebra/models/diffusion/unet_diffusion.py
+++ ice_station_zebra/models/diffusion/unet_diffusion.py
@@ -75,7 +75,8 @@ class UNetDiffusion(nn.Module):
 
         # Encoder
         self.conv1 = CommonConvBlock(
-            in_channels=input_channels + output_channels, #input_channels (n_input_days * n_input_channels), output_channels (n_output_classes * n_forecast_days)
+            in_channels=input_channels
+            + output_channels,  # input_channels (n_input_days * n_input_channels), output_channels (n_output_classes * n_forecast_days)
             out_channels=channels[0],
             kernel_size=kernel_size,
             norm_type=normalization,

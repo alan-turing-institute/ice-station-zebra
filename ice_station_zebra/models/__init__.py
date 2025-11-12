df2561e99386471376bd407032cce7232cca3401
Maria Novitasari <mnovitasari@turing.ac.uk>
2025-11-12 13:00:16 +0000

    :wrench: Update init

--- ice_station_zebra/models/__init__.py
+++ ice_station_zebra/models/__init__.py
@@ -1,9 +1,11 @@
 from .encode_process_decode import EncodeProcessDecode
 from .persistence import Persistence
 from .zebra_model import ZebraModel
+from .ddpm import DDPM
 
 __all__ = [
     "EncodeProcessDecode",
     "Persistence",
     "ZebraModel",
+    "DDPM",
 ]

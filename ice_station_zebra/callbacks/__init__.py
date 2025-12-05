8eb30ede74d24df6eea06dcdc466f811406f25ac
erinuclkwon <erinquan0521@gmail.com>
2025-12-05 12:57:19 +0000

    add config for callback

--- ice_station_zebra/callbacks/__init__.py
+++ ice_station_zebra/callbacks/__init__.py
@@ -2,12 +2,12 @@ from .ema_weight_averaging_callback import EMAWeightAveragingCallback
 from .metric_summary_callback import MetricSummaryCallback
 from .plotting_callback import PlottingCallback
 from .unconditional_checkpoint import UnconditionalCheckpoint
-from .wandb_metric_callback import WandbMetricConfig
+from .wandb_metric_callback import WandbMetric
 
 __all__ = [
     "EMAWeightAveragingCallback",
     "MetricSummaryCallback",
     "PlottingCallback",
     "UnconditionalCheckpoint",
-    "WandbMetricConfig",
+    "WandbMetric",
 ]
--- ice_station_zebra/callbacks/wandb_metric_callback.py
+++ ice_station_zebra/callbacks/wandb_metric_callback.py
@@ -3,10 +3,11 @@ from lightning.pytorch import LightningModule, Trainer
 from lightning.pytorch.callbacks import Callback
 
 
-class WandbMetricConfig(Callback):
+class WandbMetric(Callback):
     """Configure Wandb to plot train and val losses together."""
 
     def on_train_start(self, _trainer: Trainer, _pl_module: LightningModule) -> None:
         """Configure metric grouping when training starts."""
         if wandb.run is not None:
-            wandb.define_metric("loss/*", step_metric="epoch")
+            wandb.define_metric("train_loss", step_metric="epoch")
+            wandb.define_metric("validation_loss", step_metric="epoch")

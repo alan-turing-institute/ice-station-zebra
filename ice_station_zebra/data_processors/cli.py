b4f2b107a2c9f18ea94c3a009b1d0787fd5a82d1
Isabel Fenton <IFenton@users.noreply.github.com>
2025-12-12 16:07:28 +0000

    Fixing download issue for full dataset (#143)
    * :wrench: Adding option to overwrite existing datasets
    
    * :sparkles: Add the init function to initialise a dataset
    
    * :rotating_light: linting
    
    * :art: Adding the overwrite argument to the init function
    
    * :art: Adding in load / finalise from anemoi datasets
    
    * :rotating_light: linting
    
    * :loud_sound: Adding log of which part is being run in load
    
    * :pencil2: Updating part to parts
    
    * :bug: Increasing the extreme input in the bounded test to reduce the chance of it failing accidentally
    
    * :memo: Adding details of how to download large datasets to the README
    
    * :rotating_light: Linting
    
    * :memo: Updating the README with more detail
    
    * :white_check_mark: Updating TestDatasetsCLI to handle new commands
    
    * 147 Automated Multi-Part Downloading (#148)
    
    * add function to compute total chunks from the dates
    
    * Testing the data processor chunking
    
    * Fix circular import
    
    * Create temporary file to track part downloading
    
    * Test file creation and failures
    
    * Change precommit rule
    
    * Loading in parts automation function and making it safe with external tracking file
    
    * Update tests for data processing
    
    * Tests for time outs, resume, locking behaviours
    
    * Add to CLI
    
    * Include dependency
    
    * Add more options for the CLI
    
    * Options for CLI
    
    * Override for number of parts
    
    * Example part number in data yaml
    
    * Update readme
    
    * Add tests for overwrite and reset
    
    * Fix mypy issues
    
    * Update readme
    
    * :recycle: change force-overwrite to overwrite for consistency with other commands
    
    * :recycle: Setting default parts to 10
    
    * :recycle: Always re-run in progress
    
    * :rotating_light: Linting
    
    * :recycle: Don't run if the dataset was downloaded with create
    
    * :white_check_mark: Updating TestDatasetsCLI to handle new commands
    
    * :recycle: removing resume as it was doing the same as force-reset
    
    * :recycle: List datasets not loaded if using dataset command
    
    * :recycle: Removing option of setting parts in local config as it wasn't functioning
    
    ---------
    
    Co-authored-by: Isabel Fenton <ifenton@turing.ac.uk>
    
    ---------
    
    Co-authored-by: Lydia France <85945427+LydiaFrance@users.noreply.github.com>

--- ice_station_zebra/data_processors/cli.py
+++ ice_station_zebra/data_processors/cli.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Annotated
 
 import typer
 from omegaconf import DictConfig
@@ -16,13 +17,19 @@ logger = logging.getLogger(__name__)
 
 @datasets_cli.command("create")
 @hydra_adaptor
-def create(config: DictConfig) -> None:
+def create(
+    config: DictConfig,
+    *,
+    overwrite: Annotated[
+        bool, typer.Option(help="Specify whether to overwrite existing datasets")
+    ] = False,
+) -> None:
     """Create all datasets."""
     register_filters()
     factory = ZebraDataProcessorFactory(config)
     for dataset in factory.datasets:
         logger.info("Working on %s.", dataset.name)
-        dataset.create()
+        dataset.create(overwrite=overwrite)
 
 
 @datasets_cli.command("inspect")
@@ -33,6 +40,91 @@ def inspect(config: DictConfig) -> None:
     for dataset in factory.datasets:
         logger.info("Working on %s.", dataset.name)
         dataset.inspect()
+
+
+@datasets_cli.command("init")
+@hydra_adaptor
+def init(
+    config: DictConfig,
+    *,
+    overwrite: Annotated[
+        bool, typer.Option(help="Specify whether to overwrite existing datasets")
+    ] = False,
+) -> None:
+    """Create all datasets."""
+    register_filters()
+    factory = ZebraDataProcessorFactory(config)
+    for dataset in factory.datasets:
+        logger.info("Working on %s.", dataset.name)
+        dataset.init(overwrite=overwrite)
+
+
+@datasets_cli.command("load")
+@hydra_adaptor
+def load(
+    config: DictConfig,
+    parts: Annotated[str, typer.Option(help="The part to process, specified as 'i/n'")],
+) -> None:
+    """Load dataset in parts."""
+    register_filters()
+    factory = ZebraDataProcessorFactory(config)
+    for dataset in factory.datasets:
+        logger.info("Working on %s.", dataset.name)
+        dataset.load(parts=parts)
+
+
+@datasets_cli.command("load_in_parts")
+@hydra_adaptor
+def load_in_parts(
+    config: DictConfig,
+    *,
+    continue_on_error: Annotated[
+        bool, typer.Option(help="Continue to next part on error")
+    ] = True,
+    force_reset: Annotated[
+        bool,
+        typer.Option(
+            help="Clear existing progress part_tracker file and start from part 1"
+        ),
+    ] = False,
+    dataset: Annotated[
+        str | None, typer.Option(help="Run only a single dataset by name")
+    ] = None,
+    total_parts: Annotated[
+        int, typer.Option(help="Override default total parts (10)")
+    ] = 10,
+    overwrite: Annotated[
+        bool,
+        typer.Option(help="Delete the dataset directory before loading"),
+    ] = False,
+) -> None:
+    """Load all parts for all datasets in parts, recording progress so runs can be resumed."""
+    register_filters()
+    factory = ZebraDataProcessorFactory(config)
+    for ds in factory.datasets:
+        if dataset and ds.name != dataset:
+            logger.info("Not loading %s.", ds.name)
+            continue
+        logger.info("Working on %s.", ds.name)
+        ds.load_in_parts(
+            continue_on_error=continue_on_error,
+            force_reset=force_reset,
+            total_parts=total_parts,
+            overwrite=overwrite,
+        )
+
+
+@datasets_cli.command("finalise")
+@hydra_adaptor
+def finalise(
+    config: DictConfig,
+) -> None:
+    """Finalise loaded dataset."""
+    register_filters()
+    factory = ZebraDataProcessorFactory(config)
+    for dataset in factory.datasets:
+        logger.info("Working on %s.", dataset.name)
+        dataset.finalise()
 
 
 if __name__ == "__main__":

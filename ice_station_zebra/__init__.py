9ef1c7e0fca6e6d2944ac00e6a62edbfb7623dcb
James Robinson <james.em.robinson@gmail.com>
2026-02-02 15:34:07 +0000

    :rotating_light: Add missing stack_level argument to warning

--- ice_station_zebra/__init__.py
+++ ice_station_zebra/__init__.py
@@ -7,6 +7,7 @@ if "torch" in sys.modules:
     warnings.warn(
         "PYTORCH_ENABLE_MPS_FALLBACK was set after torch was imported."
         "This means that it might not have any effect. "
-        "If you have problems, please try setting this environment variable before"
-        "importing torch."
+        "If you have problems, please try setting this environment variable before "
+        "importing torch.",
+        stacklevel=2,
     )

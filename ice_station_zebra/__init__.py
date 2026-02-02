be205c05c82399f9459f0823ba91ec33d23a2b83
James Robinson <james.em.robinson@gmail.com>
2026-02-02 15:23:26 +0000

    :loud_sound: Add warning if torch is imported before setting MPS fallback

--- ice_station_zebra/__init__.py
+++ ice_station_zebra/__init__.py
@@ -1,4 +1,12 @@
-# We set this environment variable here so that it is set before we load torch
 import os
+import sys
+import warnings
 
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+if "torch" in sys.modules:
+    warnings.warn(
+        "PYTORCH_ENABLE_MPS_FALLBACK was set after torch was imported."
+        "This means that it might not have any effect. "
+        "If you have problems, please try setting this environment variable before"
+        "importing torch."
+    )

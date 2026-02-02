0bef2ab70d7be6ae7175952477ebbeb750b9c5a2
James Robinson <james.em.robinson@gmail.com>
2026-02-02 16:01:31 +0000

    :memo: Fix spacing in log message

--- ice_station_zebra/__init__.py
+++ ice_station_zebra/__init__.py
@@ -5,7 +5,7 @@ import warnings
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 if "torch" in sys.modules:
     warnings.warn(
-        "PYTORCH_ENABLE_MPS_FALLBACK was set after torch was imported."
+        "PYTORCH_ENABLE_MPS_FALLBACK was set after torch was imported. "
         "This means that it might not have any effect. "
         "If you have problems, please try setting this environment variable before "
         "importing torch.",

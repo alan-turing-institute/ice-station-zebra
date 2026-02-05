fbfe6fc1f6d9b6a4709003447e9a7e5bb2cb985f
Maria Novitasari <mnovitasari@turing.ac.uk>
2026-02-05 12:52:05 +0000

    :bento: Add simple 2d encoder class

--- ice_station_zebra/models/ddpm.py
+++ ice_station_zebra/models/ddpm.py
@@ -27,6 +27,18 @@ os.environ["SLURM_NTASKS_PER_NODE"] = "1"
 torch.set_default_dtype(torch.float32)
 
 
+class SimpleEncoder2D(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            torch.nn.GroupNorm(4, out_channels),
+            torch.nn.SiLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 class DDPM(ZebraModel):
     """Denoising Diffusion Probabilistic Model (DDPM).
 

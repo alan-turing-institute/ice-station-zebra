afea83d2fb08c463e27575d45b35ec8e258c3f68
James Robinson <james.em.robinson@gmail.com>
2026-02-06 17:05:06 +0000

    :coffin: Removed raw inputs callback and config

--- ice_station_zebra/callbacks/__init__.py
+++ ice_station_zebra/callbacks/__init__.py
@@ -1,7 +1,6 @@
 from .ema_weight_averaging_callback import EMAWeightAveragingCallback
 from .metric_summary_callback import MetricSummaryCallback
 from .plotting_callback import PlottingCallback
-from .raw_inputs_callback import RawInputsCallback
 from .unconditional_checkpoint import UnconditionalCheckpoint
 from .wandb_metric_callback import WandbMetric
 
@@ -9,7 +8,6 @@ __all__ = [
     "EMAWeightAveragingCallback",
     "MetricSummaryCallback",
     "PlottingCallback",
-    "RawInputsCallback",
     "UnconditionalCheckpoint",
     "WandbMetric",
 ]

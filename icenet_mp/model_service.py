7ce28e77f6b9c470bfa6e39d54492f3f1bc6837c
James Robinson <james.em.robinson@gmail.com>
2026-02-10 13:04:17 +0000

    :sparkles: Add ModelService utility class

---
+++ icenet_mp/model_service.py
@@ -0,0 +1,264 @@
+import logging
+from pathlib import Path, PosixPath
+from typing import TYPE_CHECKING, Protocol, cast, runtime_checkable
+
+import hydra
+import torch
+from lightning import Callback, Trainer
+from lightning.fabric.utilities import suggested_max_num_workers
+from lightning.pytorch.callbacks import ModelCheckpoint
+from omegaconf import DictConfig, OmegaConf
+
+from icenet_mp.callbacks import UnconditionalCheckpoint
+from icenet_mp.data_loaders import CommonDataModule
+from icenet_mp.models.base_model import BaseModel
+from icenet_mp.utils import (
+    generate_run_name,
+    get_device_name,
+    get_device_threads,
+    get_timestamp,
+    get_wandb_logger,
+)
+
+if TYPE_CHECKING:
+    from lightning.pytorch.loggers import Logger as LightningLogger
+
+logger = logging.getLogger(__name__)
+
+
+@runtime_checkable
+class SupportsMetadata(Protocol):
+    def set_metadata(self, config: DictConfig, model_name: str) -> None: ...
+
+
+class ModelService:
+    def __init__(self, config: DictConfig) -> None:
+        """Initialize the model service."""
+        self.config_ = config
+        self.data_module_: CommonDataModule | None = None
+        self.model_: BaseModel | None = None
+        self.trainer_: Trainer | None = None
+        self.extra_callbacks_: list[Callback] = []
+        self.extra_loggers_: list[LightningLogger] = []
+        self.run_directory_: Path | None = None
+
+    @classmethod
+    def from_config(cls, config: DictConfig) -> "ModelService":
+        """Build a new ModelService by loading a model from a configuration file."""
+        # Load the model configuration
+        builder = ModelService(config)
+
+        # Construct the model
+        builder.model_ = hydra.utils.instantiate(
+            dict(
+                {
+                    "input_spaces": [
+                        s.to_dict() for s in builder.data_module.input_spaces
+                    ],
+                    "n_forecast_steps": builder.data_module.n_forecast_steps,
+                    "n_history_steps": builder.data_module.n_history_steps,
+                    "output_space": builder.data_module.output_space.to_dict(),
+                    "optimizer": config["train"]["optimizer"],
+                    "scheduler": config["train"]["scheduler"],
+                },
+                **config["model"],
+            ),
+            _recursive_=False,
+            _convert_="object",
+        )
+
+        # Return the builder
+        return builder
+
+    @classmethod
+    def from_checkpoint(cls, checkpoint_path: Path) -> "ModelService":
+        """Build a new ModelService by loading a model from a checkpoint."""
+        # Verify the checkpoint path
+        if checkpoint_path.exists():
+            logger.debug("Found checkpoint at %s.", checkpoint_path)
+        else:
+            msg = f"Checkpoint file {checkpoint_path} does not exist."
+            raise FileNotFoundError(msg)
+
+        # Load the model configuration
+        config_path = checkpoint_path.parent.parent / "model_config.yaml"
+        try:
+            builder = ModelService(DictConfig(OmegaConf.load(config_path)))
+            logger.debug("Loaded checkpoint configuration from %s.", builder.config_)
+        except (NotADirectoryError, FileNotFoundError) as exc:
+            msg = f"Could not load checkpoint configuration from {config_path}."
+            raise FileNotFoundError(msg) from exc
+
+        # Load the model from checkpoint
+        model_cls: type[BaseModel] = hydra.utils.get_class(
+            builder.config["model"]["_target_"]
+        )
+        with torch.serialization.safe_globals([PosixPath]):
+            builder.model_ = model_cls.load_from_checkpoint(
+                checkpoint_path=checkpoint_path
+            )
+
+        return builder
+
+    @property
+    def config(self) -> DictConfig:
+        """Get the model configuration."""
+        if not self.config_:
+            msg = "Model config has not been initialised."
+            raise AttributeError(msg)
+        return self.config_
+
+    @property
+    def data_module(self) -> CommonDataModule:
+        """Get the data module instance."""
+        if not self.data_module_:
+            self.data_module_ = CommonDataModule(self.config)
+        return self.data_module_
+
+    @property
+    def model(self) -> BaseModel:
+        """Get the model instance."""
+        if not self.model_:
+            msg = "Model has not been initialised."
+            raise AttributeError(msg)
+        return self.model_
+
+    @property
+    def run_directory(self) -> Path:
+        """Get run directory from wandb logger or generate a new one."""
+        if not self.run_directory_:
+            logger.debug("Determining run directory.")
+            if wandb_logger := get_wandb_logger(self.extra_loggers_):
+                self.run_directory_ = Path(wandb_logger.experiment._settings.sync_dir)
+            else:
+                self.run_directory_ = (
+                    self.data_module.base_path
+                    / "training"
+                    / "local"
+                    / generate_run_name()
+                )
+            self.run_directory_.mkdir(parents=True, exist_ok=True)
+        return self.run_directory_
+
+    @property
+    def trainer(self) -> Trainer:
+        """Create a new Trainer or return the existing one."""
+        if not self.trainer_:
+            # Create a new Trainer
+            logger.debug("Instantiating lightning trainer.")
+            self.trainer_ = cast(
+                "Trainer",
+                hydra.utils.instantiate(
+                    dict(
+                        {
+                            "callbacks": self.extra_callbacks_,
+                            "logger": self.extra_loggers_,
+                        },
+                        **self.config["train"]["trainer"],
+                    )
+                ),
+            )
+            # Assign workers for data loading
+            self.data_module.assign_workers(
+                suggested_max_num_workers(self.trainer_.num_devices)
+            )
+        return self.trainer_
+
+    def add_callbacks(self, callback_configs: list[DictConfig]) -> None:
+        """Add extra lightning callbacks."""
+        self.extra_callbacks_ = [
+            hydra.utils.instantiate(callback_config)
+            for callback_config in callback_configs
+        ]
+
+    def add_loggers(self, overrides: dict[str, str]) -> None:
+        """Add extra lightning loggers."""
+        self.extra_loggers_ = [
+            hydra.utils.instantiate(dict(**logger_config) | overrides)
+            for logger_config in self.config.get("loggers", {}).values()
+        ]
+
+    def configure_trainer(
+        self, *, callback_configs: list[DictConfig], logger_overrides: dict[str, str]
+    ) -> None:
+        """Configure the trainer with callbacks and loggers."""
+        # Setup callbacks first
+        self.add_callbacks(callback_configs)
+        if not self.extra_callbacks_:
+            logger.warning("No callbacks have been set for the trainer.")
+
+        # Setup lightning loggers
+        self.add_loggers(logger_overrides)
+        if not self.extra_loggers_:
+            logger.warning("No loggers have been set for the trainer.")
+
+        # Additional configuration for callbacks
+        for callback in cast("list[Callback]", self.trainer.callbacks):  # type: ignore[attr-defined]
+            logger.debug("Configuring callback %s.", callback.__class__.__name__)
+            # Set metadata for supported callbacks
+            if isinstance(callback, SupportsMetadata):
+                logger.debug("Setting metadata for %s.", callback.__class__.__name__)
+                callback.set_metadata(self.config, self.model.__class__.__name__)
+            # Set checkpoint run directory for supported callbacks
+            if isinstance(callback, (ModelCheckpoint, UnconditionalCheckpoint)):
+                logger.debug(
+                    "Setting run_directory for %s to %s.",
+                    callback.__class__.__name__,
+                    self.run_directory / "checkpoints",
+                )
+                callback.dirpath = self.run_directory / "checkpoints"
+
+        # Save model config to the run directory
+        OmegaConf.save(self.config, self.run_directory / "model_config.yaml")
+
+    def evaluate(self) -> None:
+        """Evaluate a trained model."""
+        # Configure the trainer with evaluation callbacks and loggers
+        logger.info("Configuring model for evaluation.")
+        self.configure_trainer(
+            callback_configs=self.config.get("evaluate", {})
+            .get("callbacks", {})
+            .values(),
+            logger_overrides={
+                "job_type": "evaluate",
+                "name": f"{self.model.name}-{get_timestamp()}",
+                "project": "leaderboard",
+            },
+        )
+        # Log evaluation details
+        logger.info(
+            "Starting evaluation using %d threads across %d %s device(s).",
+            get_device_threads(),
+            self.trainer.num_devices,
+            get_device_name(self.trainer.accelerator.name()),
+        )
+
+        # Evaluate the model
+        self.trainer.test(
+            model=self.model,
+            datamodule=self.data_module,
+        )
+
+    def train(self) -> None:
+        """Train a model."""
+        # Configure the trainer with training callbacks and loggers
+        logger.info("Configuring model for training.")
+        self.configure_trainer(
+            callback_configs=self.config.get("train", {}).get("callbacks", {}).values(),
+            logger_overrides={"job_type": "train", "project": self.model.name},
+        )
+
+        # Log training details
+        logger.info(
+            "Starting training for %d epochs using %d threads across %d %s device(s).",
+            self.trainer.max_epochs,
+            get_device_threads(),
+            self.trainer.num_devices,
+            get_device_name(self.trainer.accelerator.name()),
+        )
+
+        # Train the model
+        self.trainer.fit(
+            model=self.model,
+            datamodule=self.data_module,
+        )

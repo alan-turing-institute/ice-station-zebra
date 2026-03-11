dbffb2ae422977fe7c625b580534e9cde009f2d2
Isabel Fenton <ifenton@turing.ac.uk>
2026-03-11 16:45:47 +0000

    :construction: Working on building an argo source

--- icenet_mp/data_processors/sources/__init__.py
+++ icenet_mp/data_processors/sources/__init__.py
@@ -3,18 +3,24 @@ import logging
 from anemoi.datasets.create.sources import source_registry
 
 from .ftp import FTPSource
+from .argo import ArgoSource
 
 logger = logging.getLogger(__name__)
 
 
 def register_sources() -> None:
     """Register all sources with anemoi-datasets."""
-    if "ftp" not in source_registry.registered:
-        source_registry.register("ftp", FTPSource)
-        logger.debug("Registered FTPSource with anemoi-datasets.")
+    sources = {"ftp": FTPSource,
+        "argo": ArgoSource,
+    }
+    for name, source in sources.items():
+        if name not in source_registry.registered:
+            source_registry.register(name, source)
+            logger.debug(f"Registered {source.__class__.__name__} with anemoi-datasets.")
 
 
 __all__ = [
     "FTPSource",
+    "ArgoSource",
     "register_sources",
 ]
---
+++ icenet_mp/data_processors/sources/argo.py
@@ -0,0 +1,159 @@
+from ftplib import FTP
+import logging
+from pathlib import Path
+import sys
+from tempfile import TemporaryDirectory
+from typing import Any
+
+from argopy import DataFetcher
+import earthkit.data as ekd
+from anemoi.datasets.create.sources import source_registry
+from anemoi.datasets.create.sources.legacy import LegacySource
+from anemoi.datasets.create.sources.xarray import load_one
+from anemoi.datasets.dates.groups import GroupOfDates
+from earthkit.data.core.fieldlist import FieldList, MultiFieldList
+from earthkit.data.utils.patterns import Pattern
+from argopy import DataFetcher
+import h5netcdf
+from networkx import sigma
+import numpy as np
+
+from icenet_mp.utils import to_list
+
+
+@source_registry.register("argo")
+class ArgoSource(LegacySource):
+    @staticmethod
+    def _execute(
+        context: dict[str, Any],
+        date_group: GroupOfDates,
+        area: str, # "90/-180/0/180" in N, W, S, E format
+    ) -> ekd.FieldList:
+        """Execute the data loading process from an FTP source."""
+        north, west, south, east = map(float, area.split("/"))
+        if (north < south) or (east < west):
+            raise ValueError(f"Invalid area: {area}. Expected format: 'N/W/S/E'")
+        
+        # create gridded variable data
+        lats = np.arange(south + 0.5, north + 0.5, 10)
+        lons = np.arange(west + 0.5, east + 0.5, 10)
+        times = date_group.dates  # unique time steps in the data
+        
+        for date in date_group.dates:
+            logging.info(f"Processing date: {date}")
+            start_date = date.replace(hour=11, minute=55, second=0)
+            end_date = date.replace(hour=12, minute=5, second=59)
+            
+            logging.info(f"Fetching Argo data for area {area} and dates from {date_group}")
+            logging.info(f"date_group.dates: {date_group.dates}")
+            logging.info(f"North: {north}, West: {west}, South: {south}, East: {east}")
+            logging.info(f"Start date: {start_date}, End date: {end_date}")
+            logging.info(f"type of dates: {type(start_date)}, {type(end_date)}")
+                
+            fetcher = DataFetcher().region([west, east, south, north, 0, 100, start_date, end_date]) # check depth of Mixed layer
+            df = fetcher.to_dataframe()
+            
+            # df['Date'] = df['TIME'].dt.date
+            logging.info(f"Fetched {df} Argo profiles")
+            sigma = 100
+        
+            for lat in lats:
+                for lon in lons:
+                    logging.info(f"Processing grid cell at lat: {lat}, lon: {lon}")
+                    t_weighted, sum_weights = 0, 0
+                    for row in df.itertuples():
+                        distance2 = (row.LATITUDE - lat) ** 2 + (row.LONGITUDE - lon) ** 2
+                        # calculate the weighted distcance
+                        weight = np.exp(-0.5 * distance2 / sigma**2)
+                        t_weighted += weight * row.TEMP
+                        sum_weights += weight
+                    logging.info(f"Sum of weights for lat: {lat}, lon: {lon} is {sum_weights}")
+                    logging.info(f"Weighted sum of TEMP for lat: {lat}, lon: {lon} is {t_weighted}")
+                    t_weighted /= sum_weights
+                    logging.info(f"Gridded value at lat: {lat}, lon: {lon} is {t_weighted}")
+        print(f"lat_range: {lats}")
+        print(f"lon_range: {lons}")
+        print(f"time_range: {times}")
+        
+        # put these into a dataframe of lat * long * time and save as netcdf
+        
+        # make these into a temporary netcdf 
+        
+        with h5netcdf.File("mydata.nc", "w") as f:
+            # set dimensions with a dictionary
+            # f.dimensions = {"lat": [-1, 0, 1], "lon": [2, 3, 4], "time": date_group.dates}  
+            f.dimensions = {"lat": len(lats), "lon": len(lons), "time": len(times)}  
+            # and update them with a dict-like interface
+            # f.dimensions['x'] = 5
+            # f.dimensions.update({'x': 5})
+
+            v_lat = f.create_variable("lat", ("lat",), float)
+            v_lat[:] = lats
+
+            v_lon = f.create_variable("lon", ("lon",), float)
+            v_lon[:] = lons
+            
+            v_time = f.create_variable("time", ("time",), np.datetime64)
+            v_time[:] = times
+
+            v = f.create_variable("TEMP", ("lat", "lon", "time"), float)
+            v[:] = np.ones((len(lats), len(lons), len(times)))
+
+        nc = load_one("📂", context, date_group.dates, "mydata.nc")
+        logging.info(f"Loaded dataset with {nc} fields")
+        
+        return MultiFieldList([nc])
+        
+        
+            # you don't need to create groups first
+            # you also don't need to create dimensions first if you supply data
+            # with the new variable
+            # v = f.create_variable("/grouped/data", ("y",), data=np.arange(10))
+
+            # access and modify attributes with a dict-like interface
+            # v.attrs["foo"] = "bar"
+
+            # you can access variables and groups directly using a hierarchical
+            # keys like h5py
+            # print(f["/grouped/data"])
+
+            # add an unlimited dimension
+            # f.dimensions["z"] = None
+            # explicitly resize a dimension and all variables using it
+            # f.resize_dimension("z", 3)
+
+        
+        # # # Parse the FTP URL
+        # # server, path_pattern = url.replace("ftp://", "").split("/", 1)
+
+        # # # Get list of remote file paths
+        # # remote_paths = {
+        # #     date.isoformat(): to_list(
+        # #         Pattern(path_pattern).substitute(date=date, allow_extra=True)
+        # #     )
+        # #     for date in dates
+        # # }
+
+        # # # Connect to the FTP server
+        # # downloaded_files: list[FieldList] = []
+        # # with TemporaryDirectory() as tmpdir, FTP(server) as session:  # noqa: S321
+        # #     base_path = Path(tmpdir)
+        # #     session.login(user=user, passwd=passwd)
+
+        # #     # Iterate over remote paths
+        # #     for iso_date, remote_path_list in remote_paths.items():
+        # #         for remote_path in remote_path_list:
+        # #             directory, filename = remote_path.rsplit("/", 1)
+        # #             session.cwd(("/" + directory).replace("//", "/"))
+        # #             local_path = base_path / filename
+        # #             # Download the remote file
+        # #             with local_path.open("wb") as local_file:
+        # #                 session.retrbinary(f"RETR {filename}", local_file.write)
+        # #             downloaded_files.append(
+        # #                 load_one("📂", context, [iso_date], str(local_path))
+        # #             )
+
+        # # Combine all downloaded files into a MultiFieldList
+        # return MultiFieldList(downloaded_files)
+        
+        sys.exit(0)

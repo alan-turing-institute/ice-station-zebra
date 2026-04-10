7f43436be0eb779cf4212ca4aee03eb05783b869
James Robinson <james.em.robinson@gmail.com>
2026-04-10 11:42:30 +0100

    :sparkles: Add nearest neighbours calculation to ReprojectionEncoder

--- icenet_mp/models/encoders/reprojection_encoder.py
+++ icenet_mp/models/encoders/reprojection_encoder.py
@@ -1,8 +1,18 @@
+import logging
+import time
+from functools import cache
+from itertools import product
 from typing import Any
+
+import numpy as np
+import torch
+from haversine import haversine_vector
 
 from icenet_mp.types import TensorNCHW
 
 from .base_encoder import BaseEncoder
+
+logger = logging.getLogger(__name__)
 
 
 class ReprojectionEncoder(BaseEncoder):
@@ -32,6 +42,101 @@ class ReprojectionEncoder(BaseEncoder):
         self.output_latitudes: list[float] = []
         self.output_longitudes: list[float] = []
 
+        # Add a cached method for calculating nearest neighbours
+        # Adding this as a decorator would lead to memory leaks
+        self.cached_nearest_neighbours = cache(self.nearest_neighbours)
+
+    def nearest_neighbours(
+        self, device: torch.device
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Calculate the nearest neighbour input cell for each cell in the output grid.
+
+        Returns:
+            Tuple of (nn_indices_h, nn_indices_w) where each is a tensor of shape
+            [output_height, output_width] containing, for each output cell, the index in
+            the H and W dimensions of the nearest neighbour input cell that should be
+            used as the source.
+
+        """
+        # Validate that provided latitudes and longitudes are consistent with expected sizes from data spaces
+        if (
+            not self.input_latitudes
+            or not self.input_longitudes
+            or not self.output_latitudes
+            or not self.output_longitudes
+        ):
+            msg = "Input/output latitudes and longitudes must be set before calculating nearest neighbours."
+            raise ValueError(msg)
+        if len(self.input_latitudes) != self.data_space_in.area:
+            msg = f"Number of input latitudes {len(self.input_latitudes)} does not match expected size from data space {self.data_space_in.shape}"
+            raise ValueError(msg)
+        if len(self.input_longitudes) != self.data_space_in.area:
+            msg = f"Number of input longitudes {len(self.input_longitudes)} does not match expected size from data space {self.data_space_in.shape}"
+            raise ValueError(msg)
+        if len(self.output_latitudes) != self.data_space_out.area:
+            msg = f"Number of output latitudes {len(self.output_latitudes)} does not match expected size from data space {self.data_space_out.shape}"
+            raise ValueError(msg)
+        if len(self.output_longitudes) != self.data_space_out.area:
+            msg = f"Number of output longitudes {len(self.output_longitudes)} does not match expected size from data space {self.data_space_out.shape}"
+            raise ValueError(msg)
+
+        # Get all indices for each cell in the input grid
+        input_indices = list(
+            product(
+                range(self.data_space_in.shape[0]), range(self.data_space_in.shape[1])
+            )
+        )
+
+        # Get lat/lon values for each cell in the input and output grids
+        input_latlons = np.array(
+            list(zip(self.input_latitudes, self.input_longitudes, strict=False)),
+            dtype=np.float32,
+        )
+        output_latlons = np.array(
+            list(zip(self.output_latitudes, self.output_longitudes, strict=False)),
+            dtype=np.float32,
+        )
+
+        # We record the time taken in reprojection as this can be slow
+        start = time.perf_counter()
+        logger.warning(
+            "Calculating reprojection from %d input grid points to %d output grid points...",
+            len(input_latlons),
+            len(output_latlons),
+        )
+
+        # We want to find the closest input grid point for each output grid point. If we
+        # try to fully vectorise the call, generating a single array of shape
+        # [data_space_out.area, data_space_in.area], then we will run out of memory.
+        # Instead we loop over the output points, using argmin to reduce to the closest
+        # source point for each output point. We then look up the source grid indices
+        # for that source point and store each of the height and width indices in an
+        # array of [data_space_out_h, data_space_out_w]. This allows easy application of
+        # the index lookup during the forward pass.
+        closest_src_point_indices = np.array(
+            [
+                input_indices[
+                    np.argmin(haversine_vector(input_latlons, output_latlon, comb=True))
+                ]
+                for output_latlon in output_latlons
+            ]
+        )  # [output_h * output_w, 2]
+        nn_indices_h = torch.tensor(
+            closest_src_point_indices[:, 0].reshape(self.data_space_out.shape),
+            dtype=torch.int,
+            device=device,
+        )  # [output_h, output_w]
+        nn_indices_w = torch.tensor(
+            closest_src_point_indices[:, 1].reshape(self.data_space_out.shape),
+            dtype=torch.int,
+            device=device,
+        )  # [output_h, output_w]
+        logger.info(
+            "Reprojection calculation took %.2f seconds", time.perf_counter() - start
+        )
+
+        return (nn_indices_h, nn_indices_w)
+
     def forward(self, x: TensorNCHW) -> TensorNCHW:
         """Forward step: encode input space into latent space by splitting into patches.
 
@@ -42,7 +147,7 @@ class ReprojectionEncoder(BaseEncoder):
             TensorNCHW with (batch_size, latent_channels, latent_height, latent_width)
 
         """
-        return x
+        return x[:, :, *self.cached_nearest_neighbours(x.device)]
 
     def set_latlon(
         self, name: str, latitudes: list[float], longitudes: list[float]

4e23460299f09a031fa30920e930bf2f4f4ffc0d
James Robinson <james.em.robinson@gmail.com>
2026-05-15 22:32:02 +0100

    :sparkles: Save metrics on a per-epoch basis

--- icenet_mp/callbacks/metric_summary_callback.py
+++ icenet_mp/callbacks/metric_summary_callback.py
@@ -1,4 +1,5 @@
 import logging
+from typing import TYPE_CHECKING
 
 import wandb
 from lightning import LightningModule, Trainer
@@ -7,24 +8,40 @@ from torchmetrics import MetricCollection
 
 from icenet_mp.utils import get_wandb_run
 
+if TYPE_CHECKING:
+    from torch import Tensor
+
 logger = logging.getLogger(__name__)
 
 
 class MetricSummaryCallback(Callback):
     """A callback to summarise metrics during evaluation."""
 
-    def log_metrics(self, trainer: Trainer, metrics: MetricCollection) -> None:
-        """Log the metrics to W&B."""
+    def log_per_epoch_metrics(
+        self, trainer: Trainer, metrics: MetricCollection
+    ) -> None:
+        """Log per-epoch metrics to W&B."""
         for name, metric in metrics.items():
-            # Compute the metric value (e.g., SIEError) across all batches and log it
-            values = metric.compute()
+            # Compute the metric value (e.g., SIEError) across all batches
+            values: Tensor = metric.compute()
 
+            # Log the mean value of the metric across all days
             for logger_ in trainer.loggers:
-                # Log the mean value of the metric across all days
-                logger_.log_metrics({f"{name} (mean)": values.mean().item()})
+                logger_.log_metrics(
+                    {
+                        f"{name} (mean)": values.mean().item(),
+                        "epoch": trainer.current_epoch,
+                    }
+                )
 
-            # check if WandB is being used as a logger and metrics are calculated for multiple days
-            # if so, log the metric values as a table and plot
+    def log_per_run_metrics(self, trainer: Trainer, metrics: MetricCollection) -> None:
+        """Log per-run metrics to W&B."""
+        for name, metric in metrics.items():
+            # Compute the metric value (e.g., SIEError) across all batches
+            values: Tensor = metric.compute()
+
+            # Check if WandB is being used as a logger and metrics are calculated for
+            # multiple days if so, log the metric values as a table and plot.
             if (
                 isinstance(run := get_wandb_run(trainer), wandb.Run)
                 and values.numel() > 1
@@ -38,16 +55,30 @@ class MetricSummaryCallback(Callback):
                     {plot_name: wandb.plot.line(table, "day", name, title=plot_name)}
                 )
 
+    def on_test_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
+        """Called at the end of a test epoch."""
+        if isinstance(pl_module.test_metrics, MetricCollection):
+            self.log_per_epoch_metrics(trainer, pl_module.test_metrics)
+        else:
+            logger.warning("Could not load train metrics!")
+
     def on_test_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         """Called at the end of testing."""
         if isinstance(pl_module.test_metrics, MetricCollection):
-            self.log_metrics(trainer, pl_module.test_metrics)
+            self.log_per_run_metrics(trainer, pl_module.test_metrics)
         else:
             logger.warning("Could not load test metrics!")
+
+    def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
+        """Called at the end of a training epoch."""
+        if isinstance(pl_module.train_metrics, MetricCollection):
+            self.log_per_epoch_metrics(trainer, pl_module.train_metrics)
+        else:
+            logger.warning("Could not load train metrics!")
 
     def on_train_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         """Called at the end of training."""
         if isinstance(pl_module.train_metrics, MetricCollection):
-            self.log_metrics(trainer, pl_module.train_metrics)
+            self.log_per_run_metrics(trainer, pl_module.train_metrics)
         else:
             logger.warning("Could not load train metrics!")

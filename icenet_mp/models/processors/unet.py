cf2b845dc3b5cae0a09f630a12166d9e29741537
James Robinson <james.em.robinson@gmail.com>
2026-05-28 16:36:45 +0100

    :bug: Explicitly pass out_channels to ConvBlockUpsampleNaive in UNetProcessor

--- icenet_mp/models/processors/unet.py
+++ icenet_mp/models/processors/unet.py
@@ -65,10 +65,10 @@ class UNetProcessor(BaseProcessor):
         self.conv5 = CommonConvBlock(channels[2], channels[3], kernel_size=kernel_size)
 
         # Decoder
-        self.up6 = ConvBlockUpsampleNaive(channels[3], channels[2])
-        self.up7 = ConvBlockUpsampleNaive(channels[2], channels[2])
-        self.up8 = ConvBlockUpsampleNaive(channels[2], channels[1])
-        self.up9 = ConvBlockUpsampleNaive(channels[1], channels[0])
+        self.up6 = ConvBlockUpsampleNaive(channels[3], out_channels=channels[2])
+        self.up7 = ConvBlockUpsampleNaive(channels[2], out_channels=channels[2])
+        self.up8 = ConvBlockUpsampleNaive(channels[2], out_channels=channels[1])
+        self.up9 = ConvBlockUpsampleNaive(channels[1], out_channels=channels[0])
 
         self.up6b = CommonConvBlock(channels[3], channels[2], kernel_size=kernel_size)
         self.up7b = CommonConvBlock(channels[3], channels[2], kernel_size=kernel_size)
